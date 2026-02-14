45796abd445af94ed6b033dfef1876e010b8869b
MatitoTM <Taitomatan2010@gmail.com>
2026-02-14 19:12:00 +0200

    The structure of the program.

--- app/src/main/java/com/example/task09/MainActivity.java
+++ app/src/main/java/com/example/task09/MainActivity.java
@@ -1,6 +1,9 @@
 package com.example.task09;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -10,10 +13,19 @@ import androidx.core.view.WindowInsetsCompat;
 
 public class MainActivity extends AppCompatActivity {
 
+    EditText eT;
+    Button btn;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        eT = findViewById(R.id.eT);
+        btn = findViewById(R.id.btn);
+    }
+
+    public void clicked(View view) {
 
     }
 }
